9d9b6c655484ef9761d3ad10bc6f0640b24d9a51
lisaokta95 <lisaokta9595@gmail.com>
2018-07-16 21:21:00 +0700

    Add files via upload

---
+++ cordova rest/README.md
@@ -0,0 +1,2 @@
+# cordova-rest
+cordova rest mysql php
---
+++ cordova rest/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id6246197_db","id6246197_db","id6246197_db") or die ("could not connect database");
+?>
